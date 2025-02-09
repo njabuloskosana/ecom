fb94919d3534f9757974bf945ad8489db66cbd39
Njabulo Skosana <49289663+njabuloskosana@users.noreply.github.com>
2025-02-09 04:58:09 +0200

    Adding home screen

--- web/frontend/src/total-client-ui/Home.vue
+++ web/frontend/src/total-client-ui/Home.vue
@@ -2,11 +2,18 @@
     <div class="welcome-container">
       <main>
         <h1>Welcome to the Portal</h1>
-      </main>
+    </main>
   
-      <footer>
-        <button class="logout-button" @click="logout">Logout</button>
-      </footer>
+    <footer class="footer">
+  <div class="user-info">
+    <div class="points"> <img src="../assets/coins.png" alt="coins" class="coins" /> {{ `3000 points` }}</div>
+   
+    <button class="logout-button" @click="logout">
+      <img src="../assets/logout.png" alt="Logout" class="logout-icon" />
+      Logout
+    </button>
+  </div>
+</footer>
     </div>
   </template>
   
@@ -55,26 +62,67 @@ export default {
     color: #333;
   }
   
-  footer {
-    background: #333;
-    color: white;
-    padding: 10px;
-    text-align: center;
-  }
-  
-  .logout-button {
-    background: #e74c3c;
-    color: white;
-    border: none;
-    padding: 10px 20px;
-    font-size: 1rem;
-    cursor: pointer;
-    border-radius: 5px;
-    transition: 0.3s;
-  }
-  
-  .logout-button:hover {
-    background: #c0392b;
-  }
+  .footer {
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  padding: 10px 20px;
+  background: #333;
+  color: white;
+  border-top: 1px solid #ddd;
+}
+
+.user-info {
+  display: flex;
+  flex-direction: row;
+align-items: center;
+ justify-items: center;
+  gap: 10px;
+  justify-content: space-between;
+  width: 100%;
+}
+
+.user-name {
+  font-size: 16px;
+  font-weight: bold;
+  color: #ffff;
+}
+
+.logout-button {
+  display: flex;
+  align-items: center;
+  gap: 5px;
+  padding: 8px 12px;
+  background-color: #ffff;
+  color: black;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  font-size: 14px;
+  transition: background 0.3s ease;
+}
+
+.logout-button:hover {
+  background-color: #c0392b;
+}
+
+.logout-icon {
+  width: 16px;
+  height: 16px;
+}
+
+.coins {
+  width: 50px;
+  height: 30px;
+}
+
+.points{
+    display: flex;
+    align-items: center;
+    gap: 5px;
+    font-size: 16px;
+    font-weight: bold;
+    color: #ffff;
+}
   </style>
   

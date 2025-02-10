71deaf98f748b16b1545d278a4891c8409f12cd6
Njabulo Skosana <49289663+njabuloskosana@users.noreply.github.com>
2025-02-10 18:23:02 +0200

    Update TicTacToe.vue

--- web/frontend/src/games/TicTacToe.vue
+++ web/frontend/src/games/TicTacToe.vue
@@ -8,10 +8,9 @@
   <input type="radio" name="cell-2" id="cell-2-x" />
   <input type="radio" name="cell-2" id="cell-2-o" />
   <input type="radio" name="cell-3" id="cell-3-x" />
-  <inadio" name="cell-3" id="cell-3-x" />
   <input type="radio" name="cell-3" id="cell-3-o" />
-  <ipunput type="radio" name="cell-4" id="cell-4-o" />
-  <inputt type="radio" name="cell-4" id="cell-4-o" />
+  <input type="radio" name="cell-4" id="cell-4-x" />
+  <input type="radio" name="cell-4" id="cell-4-o" />
   <input type="radio" name="cell-5" id="cell-5-x" />
   <input type="radio" name="cell-5" id="cell-5-o" />
   <input type="radio" name="cell-6" id="cell-6-x" />
@@ -80,7 +79,7 @@
 </template>
 
 <script setup lang="ts">
-  import { ref } from "vue";
+
 </script>
 
 <style scoped>
